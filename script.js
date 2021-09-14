b09c34eebab54e7341c65090f51e8681a1b55dcc
rahul31agrawal <rahul31agrawal@gmail.com>
2021-09-14 12:39:57 +0530

    input-processing-part-updated

---
+++ script.js
@@ -0,0 +1,27 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("check-number");
+
+
+
+
+
+
+function checkBirthDateIsLucky() {
+
+const dob = dateOfBirth.value;
+const sum = calculateSum(dob);
+console.log(sum);
+}
+
+function calculateSum(dob {
+dob = dob.replaceAll("-","");
+let sum =0;
+for(let index=0; index < dob.length;index++){
+    sum=sum + Number(dob.charAt(index)); 
+}
+return sum;
+
+}
+
+checkNumberButton.addEventListener('click', checkBirthDateIsLucky)
